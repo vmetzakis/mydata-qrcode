134d24b00f12599658a203817c468cdb9e791170
Vangelis Metzakis <55538537+vmetzakis@users.noreply.github.com>
2023-11-29 15:44:02 +0200

    Update mydataqrcode.py

--- mydataqrcode.py
+++ mydataqrcode.py
@@ -1,13 +1,17 @@
 import sys
+import os
 import qrcode
 
 def createQR(path,name,qrurl):
     img = qrcode.make(qrurl)
     type(img)
-    img.save(path+name+'.png')
+    img.save(os.path.join(path+name+'.png'))
 
 if __name__ == "__main__":
-	if sys.argv[1] !='':	
-		createQR(sys.argv[1],sys.argv[2],sys.argv[3])
+    if len(sys.argv) != 4:
+        print('Wrong arguments.Use <path> <imgname> <qrurl>')  
+        sys.exit(1)   
+		
+    createQR(sys.argv[1],sys.argv[2],sys.argv[3]) 
 
 
